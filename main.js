42598ca68c42daabb82f2cd9624e9ab9f14d236a
Daviranur <syaqinahnur05@gmail.com>
2025-02-23 17:37:47 +0700

    button hapus

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 // Do your work here...
 console.log('Hello, world!');
+
+// ini kode buat tombol hapus
+let buttonHapus = document.createElement('button');
+buttonHapus.textContent = `Hapus Buku`;
+buttonHapus.classList.add('buttons');
+buttonHapus.setAttribute('data-testid','bookItemDeleteButton');
+buttonHapus.setAttribute('data-bookId','incompleteBookList');
+buttonHapus.addEventListener('click',function(){
+    hapusBuku(data.id);
+});
+
+
+// ini fungsinya 
+function hapusBuku(id){
+    let hapus = getNewBook();
+    let idData = hapus = hapus.filter((book) => book.id !== id);
+
+    localStorage.setItem(keyStorage,JSON.stringify(hapus));
+    localStorage.removeItem(hapus);
+    renderListBook();
+};
