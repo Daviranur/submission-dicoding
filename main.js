a03dbd3c690354860c35064a692f6e491cf14c7c
Daviranur <syaqinahnur05@gmail.com>
2025-02-23 17:53:24 +0700

    button update

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 // Do your work here...
 console.log('Hello, world!');
+
+
+// ini kode buat tombol updatenya 
+let buttonUpdate = document.createElement('button');
+buttonUpdate.textContent = data.isComplete ? `Belum Selesai` : `Selesai dibaca`;
+buttonUpdate.classList.add('buttons');
+buttonUpdate.setAttribute('data-testid','bookItemIsCompleteButton');
+buttonUpdate.setAttribute('data-bookId','completeBooklist');
+buttonUpdate.addEventListener('click',function(){
+    bookStatus(data.id);
+});
+
+
+
+// ini fungsinya 
+function bookStatus(id){
+    let data = getNewBook();
+    let book = data.findIndex(book => book.id === id);
+
+    if(book !== -1){
+        data[book].isComplete = !data[book].isComplete;
+        renderListBook();
+    }
+}
